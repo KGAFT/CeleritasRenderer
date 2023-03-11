a02c28ae5637d22dee166077cb8322ecb1be3016
KGAFT <kgaftkgaft@gmail.com>
2023-03-11 16:56:55 +0300

    Added render pass, start work on sync

--- src/cxx/main.cpp
+++ src/cxx/main.cpp
@@ -3,30 +3,41 @@
 #include "Vulkan/VulkanInstance.h"
 #include "Vulkan/VulkanDevice/VulkanDevice.h"
 #include "Vulkan/VulkanSwapChain.h"
+#include "Vulkan/VulkanRenderPass.h"
 
 int main() {
     Window::initializeContext();
     int monitorCount = 0;
-    Monitor** monitors = Monitor::enumerateMonitors(&monitorCount);
-    
+    Monitor **monitors = Monitor::enumerateMonitors(&monitorCount);
+
     Window::createWindow(1024, 768, "Hello world!", nullptr);
 
     Window::getInstance()->borderless();
     Window::getInstance()->borderless();
 
     VulkanInstance instance;
-    std::cout<<instance.createInstance("CeleritasEngine", true)<<std::endl;
+    std::cout << instance.createInstance("CeleritasEngine", true) << std::endl;
     VkPhysicalDevice deviceToCreate;
-    for (const auto &item: VulkanDevice::enumerateSupportedDevices(instance.getInstance(), Window::getInstance())){
-        std::cout<<item.second.deviceName<<std::endl;
+    for (const auto &item: VulkanDevice::enumerateSupportedDevices(instance.getInstance(), Window::getInstance())) {
+        std::cout << item.second.deviceName << std::endl;
         deviceToCreate = item.first;
         break;
     }
     VulkanDevice device(deviceToCreate, Window::getInstance(), instance.getInstance(), true);
     VulkanSwapChain swapChain(&device, Window::getInstance()->getWidth(), Window::getInstance()->getHeight());
+    VkFormat format = swapChain.getSwapChainImageFormat();
+    VulkanRenderPass renderPass(&device, swapChain.getSwapChainImageViews(), Window::getInstance()->getWidth(),
+                                Window::getInstance()->getHeight(), 1, &format, 1);
+
+    
     while (!Window::getInstance()->needToClose()) {
         Window::getInstance()->preRenderEvents();
         Window::getInstance()->postRenderEvents();
     }
+    renderPass.destroy();
+    swapChain.destroy();
+    device.destroy();
+    int wait = 0;
+    std::cin>>wait;
     return 0;
 }
