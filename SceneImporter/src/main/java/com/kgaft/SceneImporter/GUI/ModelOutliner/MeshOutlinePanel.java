bac4e3cde8f6b2642224375e1a425fbce8b428de
kgaft <kgaftkgaft@gmail.com>
2023-03-27 00:34:53 +0300

    Work on mesh outliner

---
+++ SceneImporter/src/main/java/com/kgaft/SceneImporter/GUI/ModelOutliner/MeshOutlinePanel.java
@@ -0,0 +1,32 @@
+package com.kgaft.SceneImporter.GUI.ModelOutliner;
+
+import javax.swing.*;
+
+public class MeshOutlinePanel {
+    private JPanel root;
+    private JLabel positionLabel;
+    private JTextField xPosInput;
+    private JPanel positionPanel;
+    private JTextField yPosInput;
+    private JTextField zPosInput;
+    private JPanel rotationPanel;
+    private JLabel rotationLabel;
+    private JTextField xRotInput;
+    private JTextField yRotInput;
+    private JTextField zRotInput;
+    private JPanel scalePanel;
+    private JTextField xScalInput;
+    private JTextField yScalInput;
+    private JTextField zScalInput;
+    private JLabel scaleLabel;
+    private JPanel meshNamePanel;
+    private JTextField meshNameTextField;
+    private JPanel baseColorPanel;
+    private JLabel meshBaseColorLabel;
+    private JTextField xColInput;
+    private JTextField yColInput;
+    private JTextField zColInput;
+    private JTextField wColorInput;
+    private JLabel loadedTexturesList;
+    private JButton editTexturesButton;
+}
--- SceneImporter/src/main/java/com/kgaft/SceneImporter/GUI/ModelOutliner/MeshesOutliner.java
+++ SceneImporter/src/main/java/com/kgaft/SceneImporter/GUI/ModelOutliner/MeshesOutliner.java
@@ -2,7 +2,7 @@ package com.kgaft.SceneImporter.GUI.ModelOutliner;
 
 import javax.swing.*;
 
-public class MeshOutliner {
+public class MeshesOutliner {
     private JPanel root;
     private JScrollPane meshesScroller;
     private JPanel meshesContainer;
